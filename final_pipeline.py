dcad8a78f9265ac1695f80a138dd015c55772fe3
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-21 20:44:00 +0500

    added the x and y features

--- final_pipeline.py
+++ final_pipeline.py
@@ -7,3 +7,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble        import RandomForestClassifier
 import joblib
 
+insurance = pd.read_csv("F://Insurance_charges//Data//insurance.csv")
+insurance_df = insurance.copy()
+
+X = insurance_df.drop(columns=["charges"])
+y = insurance_df["charges"]
+
