2ea7641b865bcd8fc49de34893d1562c89e28251
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-21 21:07:22 +0500

    added all the input

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ import joblib
 import numpy as np
 
 model = joblib.load("tree_pipeline.joblib")
+
+age = st.number_input("Age", min_value=0.0, max_value=100.0, value=30.0)
+sex = st.selectbox("Sex", ["male", "female"])
+bmi = st.number_input("bmi", min_value=0.0, value=30.66)
+children = st.number_input("Childern", min_value=0, value=0)
+smoker = st.selectbox("Smoke?", ["yes", "no"])
+region = st.selectbox("Region: ", ["southeast", "southwest" , "northwest" , "northeast"])
