8bb4f727857c100d20bd09e831d7e7c12330ba5b
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-07-21 20:49:52 +0500

    added the preprocessing pipeline

--- final_pipeline.py
+++ final_pipeline.py
@@ -13,3 +13,15 @@ insurance_df = insurance.copy()
 X = insurance_df.drop(columns=["charges"])
 y = insurance_df["charges"]
 
+x_train , x_test , y_train , y_test = train_test_split(X , y , test_size=0.30 , random_state=42)
+
+num_columns = ["age", "bmi", "children"]
+ohe_column = ["region"]
+ordinal_columns = ["sex", "smoker"]
+
+preprocessing = ColumnTransformer([
+    ("num", "passthrough", num_columns),
+    ("ohe", OneHotEncoder(drop='first'), ohe_column),
+    ("ordinal", OrdinalEncoder(categories=[['female','male'], ['no','yes']]), ordinal_columns),
+])
+
